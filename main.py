d81db5054d20e6a13c7d38782e7b765ab923a659
dmarek <dmare13@wgu.edu>
2025-12-30 20:29:02 -0600

    updating artifacts 3

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ def go(config: DictConfig):
                 "main",
                 env_manager="conda",
                 parameters={
-                    "input_artifact": "sample.csv:v1",
-                    "output_artifact": "cleaned_sample.csv:v0",
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "cleaned_sample.csv",
                     "output_type":"cleaned_sample",
                     "output_description": "clean_sample",
                     "min_price": config["etl"]["min_price"],
